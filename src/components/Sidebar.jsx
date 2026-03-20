9184c67f47f7bff3c048271d48a7347d6eee5e64
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2026-03-20 22:20:28 +0200

    fix: add missing useEffect import in Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { version } from '../../package.json';
 import { useMailStore } from '../stores/mailStore';
 import { useThemeStore } from '../stores/themeStore';
